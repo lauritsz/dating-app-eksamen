452d156f893d62b85e550b8be2e508a3644536ca
stma20ac <stma20ac@student.cbs.dk>
2021-04-01 16:16:14 +0200

    Tester databaseconnection

--- app.js
+++ app.js
@@ -1,2 +1,38 @@
-jadaw
-egesf
+var Connection = require('tedious').Connection;
+var Request = require('tedious').Request
+
+const config = require('./config.json')
+var connection = new Connection(config)
+
+connection.on('connect', function(err){
+    if (err){
+        console.log(err);
+    } else {
+        console.log("connected");
+        //executeSQL();
+        selectData();
+    }
+});
+
+function executeSQL(){
+    request = new Request("INSERT INTO customer (customerID, FirstName) VALUES (1, 'Mogens')", function(err){
+    if (err){
+        console.log(err)}})
+
+    connection.execSql(request)
+};
+
+function selectData(){
+    request = new Request("SELECT * FROM customer", function(err){
+    if (err){
+        console.log(err)}})
+
+    connection.execSql(request)
+    console.log(request.on('row', function(columns) {
+        for(let i = 0; i < columns.length; i++) {
+            console.log(columns[i].value);
+        }
+    }));
+};
+
+connection.connect()
